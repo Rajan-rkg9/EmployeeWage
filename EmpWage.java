364c3bb2192d611eac3859ddd47d984237eb695b
Rajan-rkg9 <amirajangupta@gmail.com>
2020-09-26 12:47:17 +0530

    uc4 feature added

--- EmpWage.java
+++ EmpWage.java
@@ -1,16 +1,27 @@
 public class EmployeeWage{
+	
+	public static final int part_time = 1;
+	public static final int full_time = 2;
+	public static final int wage_per_hour = 20;
+	
 	public static void main(String[] args) {
-		int full_time = 2;
-		int part_time = 1;
-		int wage_per_hour = 20;
+		
 		int hrs = 0;
 		int wage = 0;
-		int empcheck =(int)( Math.floor(Math.random() * 10) % 3);
-		if (empcheck == full_time)
-			hrs = 8;
-		else if(empcheck == part_time)
-			hrs = 4;
-		Wage = hrs * wage_per_hour;
-			System.out.println("Employee Wage :"+Wage);
+		int empcheck = (int)(Math.floor(Math.random() * 10) % 3);
+		switch (empcheck) {
+			case part_time:
+				hrs = 4;
+				break;
+			case full_time:
+				hrs = 8;
+				break;
+			default:
+				hrs = 0;
+			
 		}
+		wage = hrs * wage_per_hour;
+			System.out.println("Employee Wage :"+wage);
+	}
 }
+
