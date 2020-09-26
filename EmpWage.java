3f1f8fc4b70ab6303933a1baf143f4b93f679aca
Rajan-rkg9 <amirajangupta@gmail.com>
2020-09-26 13:05:01 +0530

    uc6 feature added

--- EmpWage.java
+++ EmpWage.java
@@ -4,17 +4,19 @@ public class EmployeeWage{
 	public static final int full_time = 2;
 	public static final int wage_per_hour = 20;
 	public static final int working_days = 20;
+	public static final int max_hours = 100;
 	
 	public static void main(String[] args) {
 		
 		int hrs = 0;
-		int wage = 0;
-		int totalWage = 0;
+		int totalHrs = 0;
+		int totalDays = 0;
 		
-		for (int day = 0; day < working_days; day++)
+		for (; totalHrs <= max_hours && totalDays < working_days;) 
 		    {
-			int empcheck =(int)(Math.floor(Math.random() * 10) % 3);
-			switch (empcheck) {
+			totalDays++;
+			int empcheck =(int)( Math.floor(Math.random() * 10) % 3);
+			switch(empcheck) {
 			case part_time:
 				hrs = 4;
 				break;
@@ -25,11 +27,11 @@ public class EmployeeWage{
 				hrs = 0;
 			
 		}
-			wage = hrs * wage_per_hour;
-			totalWage = totalWage + wage;
-			System.out.println("Employee Wage : "+wage);
+			totalHrs = totalHrs + hrs;
+			System.out.println("Day: " + totalDays + " Employee Hours: " + hrs);
+			
 		}
-		
+		int totalWage = totalHrs * wage_per_hour;
 			System.out.println("Total Employee Wage :"+totalWage);
 	}
 }
